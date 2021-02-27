c528af0a17fc0cc4450da2ceffd2e6f9512f1049
guilhermepardo <guilherme.pardoanjos@gmail.com>
2021-02-27 20:04:24 -0300

    adicionando o projeto gerado pelo Spring Initalizr

---
+++ api-dunder-mifflin/src/main/java/com/example/apidundermifflin/ApiDunderMifflinApplication.java
@@ -0,0 +1,13 @@
+package com.example.apidundermifflin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDunderMifflinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDunderMifflinApplication.class, args);
+	}
+
+}
---
+++ api-dunder-mifflin/src/test/java/com/example/apidundermifflin/ApiDunderMifflinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apidundermifflin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDunderMifflinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
